4b17322843d57fd4c1cddf2960b18fc67d9293d3
Joshua Manani <83291945+Joshmanani@users.noreply.github.com>
2022-09-20 11:24:15 +0300

    Create cnf.js

---
+++ connect four/cnf.js
@@ -0,0 +1,8 @@
+var player1 = prompt('player One : Enter your name and you will be player One');
+var player1Color = 'rgb(86, 151, 255)';
+
+var player2 = prompt('player OnTwo : Enter your name and you will be player Two');
+var player2Color = 'rgb(86, 151, 255)';
+
+var game_on = true;
+var table = $('table tr');
